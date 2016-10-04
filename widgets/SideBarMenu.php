71a3df05cd30e90f61c2256c76bfed5515e0915d
Dedi Suhanda <dedi.suhanda@gmail.com>
2016-10-05 01:38:47 +0700

    add SideBarMenu

--- widgets/SideBarMenu.php
+++ widgets/SideBarMenu.php
@@ -2,7 +2,159 @@
 namespace yii\lte\widgets;
 
 
-class SideBarMenu
-{
+use Yii;
+use yii\bootstrap\Html;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+use yii\widgets\Menu;
 
+/**
+ * Class SideBarMenu
+ *
+ * * Menu displays a multi-level menu using nested HTML lists.
+ *
+ * The main property of Menu is [[items]], which specifies the possible items in the menu.
+ * A menu item can contain sub-items which specify the sub-menu under that menu item.
+ *
+ * Menu checks the current route and request parameters to toggle certain menu items
+ * with active state.
+ *
+ * Note that Menu only renders the HTML tags about the menu. It does do any styling.
+ * You are responsible to provide CSS styles to make it look like a real menu.
+ *
+ * The following example shows how to use Menu:
+ *
+ * ```php
+ * echo Menu::widget([
+ *     'items' => [
+ *         // Important: you need to specify url as 'controller/action',
+ *         // not just as 'controller' even if default action is used.
+ *         ['label' => 'Home', 'url' => ['site/index']],
+ *         // you can add header section with string
+ *         "Product"
+ *         // 'Products' menu item will be selected as long as the route is 'product/index'
+ *         ['label' => 'Products', 'url' => ['product/index'], 'items' => [
+ *             ['label' => 'New Arrivals', 'url' => ['product/index', 'tag' => 'new']],
+ *             ['label' => 'Most Popular', 'url' => ['product/index', 'tag' => 'popular']],
+ *         ]],
+ *         ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+ *     ],
+ * ]);
+ * ```
+ *
+ * @package yii\lte\widgets
+ * @author  Dedi Suhanda <dedi.suhanda@gmail.com>
+ */
+class SideBarMenu extends Menu
+{
+    public $linkTemplate = '<a href="{link}">{label}</a>{hasChild}';
+
+    public $hasChildTemplate = '<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>';
+
+    public $submenuTemplate = "\n<ul class=\"treeview-menu\">\n{items}\n</ul>\n";
+
+    public $encodeLabels = false;
+
+    public $activateParents = true;
+
+
+    /**
+     * Renders the menu.
+     */
+    public function run()
+    {
+        if ($this->route === null && Yii::$app->controller !== null) {
+            $this->route = Yii::$app->controller->getRoute();
+        }
+        if ($this->params === null) {
+            $this->params = Yii::$app->request->getQueryParams();
+        }
+        $items = $this->normalizeItems($this->items, $hasActiveChild);
+        if (!empty($items)) {
+            $options = $this->options;
+            Html::addCssClass($options, 'sidebar-menu');
+
+            $tag = ArrayHelper::remove($options, 'tag', 'ul');
+            echo Html::tag($tag, $this->renderItems($items), $options);
+        }
+    }
+
+    /**
+     * Recursively renders the menu items (without the container tag).
+     *
+     * @param array $items the menu items to be rendered recursively
+     *
+     * @return string the rendering result
+     */
+    protected function renderItems($items)
+    {
+        $n     = count($items);
+        $lines = [];
+        foreach ($items as $i => $item) {
+            $options  = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $hasChild = !empty($item['items']);
+            $tag   = ArrayHelper::remove($options, 'tag', 'li');
+            $class = [];
+
+            if ($hasChild) {
+                $class[] = 'treeview';
+            }
+            if ($item['active']) {
+                $class[] = $this->activeCssClass;
+            }
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+            $menu = $this->renderItem($item, $hasChild);
+            if ($hasChild) {
+                $submenuTemplate = ArrayHelper::getValue($item, 'submenuTemplate', $this->submenuTemplate);
+                $menu .= strtr($submenuTemplate, [
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+            }
+            $lines[] = Html::tag($tag, $menu, $options);
+        }
+
+        return implode("\n", $lines);
+    }
+
+    /**
+     * Renders the content of a menu item.
+     * Note that the container and the sub-menus are not rendered here.
+     *
+     * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
+     *
+     * @param       $hasChild
+     *
+     * @return string the rendering result
+     */
+    protected function renderItem($item, $hasChild)
+    {
+        if (isset($item['url'])) {
+            $template = ArrayHelper::getValue($item, 'template', $this->linkTemplate);
+
+            return strtr($template, [
+                '{url}'      => Html::encode(Url::to($item['url'])),
+                '{label}'    => $item['label'],
+                '{hasChild}' => $hasChild ? $this->hasChildTemplate : ''
+            ]);
+        } else {
+            $template = ArrayHelper::getValue($item, 'template', $this->labelTemplate);
+
+            return strtr($template, [
+                '{label}'    => $item['label'],
+                '{hasChild}' => $hasChild ? $this->hasChildTemplate : ''
+            ]);
+        }
+    }
 }
