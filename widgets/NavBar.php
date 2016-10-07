7855311c144eb855e30de65d66639fa9089a8f72
Dedi Suhanda <dedi.suhanda@gmail.com>
2016-10-08 01:44:34 +0700

    add Navbar and NavBarMenu

---
+++ widgets/NavBar.php
@@ -0,0 +1,82 @@
+<?php
+namespace yii\lte\widgets;
+
+use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * Class NavBar
+ *
+ * @package yii\lte\widgets
+ * @author  Dedi Suhanda <dedi.suhanda@gmail.com>
+ */
+class NavBar extends Widget
+{
+    public $tag           = 'nav';
+    public $options       = [];
+    public $cssClass      = 'navbar';
+    public $type          = 'static-top';
+    public $sideBarToggle = true;
+    public $toggleOptions = [];
+    public $toggleCss     = 'sidebar-toggle';
+
+    /**
+     *
+     */
+    public function init()
+    {
+        $options = [
+            'class' => $this->cssClass . ' navbar-' . $this->type,
+        ];
+        if (isset($this->options['class'])) {
+            Html::addCssClass($options, $this->options['class']);
+            unset($this->options['class']);
+        }
+        ArrayHelper::merge($options, $this->options);
+        $contents   = [];
+        $contents[] = Html::beginTag($this->tag, $options);
+
+        if ($this->sideBarToggle) {
+            $contents[] = $this->renderToggle();
+        }
+
+        echo implode("\n", $contents);
+    }
+
+    /**
+     *
+     */
+    public function run()
+    {
+        echo Html::endTag($this->tag);
+    }
+
+    /**
+     * @return array
+     */
+    public function renderToggle()
+    {
+        $toggleOptions = [
+            'class'       => $this->toggleCss,
+            'data-toggle' => 'offcanvas',
+            'role'        => 'button',
+            'href'        => '#'
+        ];
+        if (isset($this->toggleOptions['class'])) {
+            Html::addCssClass($toggleOptions, $this->toggleOptions['class']);
+            unset($this->toggleOptions['class']);
+        }
+
+        ArrayHelper::merge($toggleOptions, $this->toggleOptions);
+        $contents   = [];
+        $contents[] = Html::beginTag('a', $toggleOptions);
+        $contents[] = Html::tag('span', 'Toggle navigation', ['class' => 'sr-only']);
+        $contents[] = Html::tag('span', '', ['class' => 'icon-bar']);
+        $contents[] = Html::tag('span', '', ['class' => 'icon-bar']);
+        $contents[] = Html::tag('span', '', ['class' => 'icon-bar']);
+        $contents[] = Html::endTag('a');
+
+        return implode("\n", $contents);;
+    }
+}
---
+++ widgets/NavBarMenu.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dedi.suhanda
+ * Date: 10/7/16
+ * Time: 11:46 PM
+ */
+
+namespace yii\lte\widgets;
+
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\Menu;
+
+class NavBarMenu extends Menu
+{
+
+    public $submenuTemplate = "\n<ul class=\"dropdown-menu\">\n{items}\n</ul>\n";
+
+    public $linkTemplate = '<a href="{url}">{label}</a>';
+
+    public $encodeLabels = false;
+
+    public $labelTemplate = '{icon} <span class="{class}">{label}</span>';
+
+    public $iconTemplate = '<i class="fa fa-{icon}"></i>';
+
+
+    /**
+     * Renders the menu.
+     */
+    public function run()
+    {
+        if ($this->route === null && Yii::$app->controller !== null) {
+            $this->route = Yii::$app->controller->getRoute();
+        }
+        if ($this->params === null) {
+            $this->params = Yii::$app->request->getQueryParams();
+        }
+        $items = $this->normalizeItems($this->items, $hasActiveChild);
+
+        if (!empty($items)) {
+            $options = $this->options;
+            Html::addCssClass($options, ['nav', 'navbar-nav']);
+
+            $tag = ArrayHelper::remove($options, 'tag', 'ul');
+            echo Html::tag($tag, $this->renderItems($items), $options);
+        }
+    }
+
+    /**
+     * Recursively renders the menu items (without the container tag).
+     *
+     * @param array $items the menu items to be rendered recursively
+     *
+     * @return string the rendering result
+     */
+    protected function renderItems($items)
+    {
+        $n     = count($items);
+        $lines = [];
+        foreach ($items as $i => $item) {
+            $tag = ArrayHelper::remove($options, 'tag', 'li');
+            if ($item['isHeader']) {
+                Html::addCssClass($options, 'header');
+                $lines[] = Html::tag($tag, $this->renderItem($item), $options);
+                continue;
+            }
+
+            if (isset($item['isFooter']) && $item['isFooter']) {
+                Html::addCssClass($options, 'footer');
+                $lines[] = Html::tag($tag, $this->renderItem($item), $options);
+                continue;
+            }
+
+            $options  = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $hasChild = !empty($item['items']);
+
+            $class = [];
+
+            if ($hasChild) {
+                $class[] = 'dropdown';
+            }
+
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+            $menu = $this->renderItem($item);
+            if ($hasChild) {
+                $submenuTemplate = $this->submenuTemplate;
+                $menu .= strtr($submenuTemplate, [
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+            }
+            $lines[] = Html::tag($tag, $menu, $options);
+        }
+
+        return implode("\n", $lines);
+    }
+
+    /**
+     * Renders the content of a menu item.
+     * Note that the container and the sub-menus are not rendered here.
+     *
+     * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
+     *
+     * @return string the rendering result
+     */
+    protected function renderItem($item)
+    {
+        if ($item['isHeader'] || $item['isInnerMenu']) {
+            return $this->renderLabel($item);
+        }
+
+        if (!isset($item['url'])) {
+            $item['url'] = "#";
+        }
+        $options = [];
+
+        if (isset($item['items'])) {
+            $options = [
+                'data-toggle' => 'dropdown',
+                'class'       => 'dropdown-toggle'
+            ];
+        }
+
+        return Html::a($this->renderLabel($item), $item['url'], $options);
+
+    }
+
+    /**
+     * @param $item
+     *
+     * @return string
+     */
+    public function renderLabel($item)
+    {
+        if ($item['isInnerMenu']) {
+            return $item['label'];
+        }
+        $labelTemplate = ArrayHelper::getValue($item, 'labelTemplate', $this->labelTemplate);
+        $iconTemplate  = ArrayHelper::getValue($item, 'iconTemplate', $this->iconTemplate);
+        $icon          = '';
+
+        if (isset($item['icon'])) {
+            $icon = strtr($iconTemplate, [
+                '{icon}' => $item['icon']
+            ]);
+        }
+
+
+        $label = strtr($labelTemplate, [
+            '{icon}'  => $icon,
+            '{label}' => $item['label'],
+            '{class}' => ArrayHelper::getValue($item, 'labelClass', '')
+        ]);
+
+        return $label;
+    }
+
+
+    /**
+     * Normalizes the [[items]] property to remove invisible items and activate certain items.
+     *
+     * @param array   $items  the items to be normalized.
+     * @param boolean $active whether there is an active child menu item.
+     *
+     * @return array the normalized menu items
+     */
+    protected function normalizeItems($items, &$active)
+    {
+        foreach ($items as $i => $item) {
+            if (isset($item['visible']) && !$item['visible']) {
+                unset($items[$i]);
+                continue;
+            }
+            $items[$i]['isHeader']    = ArrayHelper::getValue($item, 'isHeader', false);
+            $items[$i]['isFooter']    = ArrayHelper::getValue($item, 'isFooter', false);
+            $items[$i]['isInnerMenu'] = ArrayHelper::getValue($item, 'isInnerMenu', false);
+
+            if (!isset($item['label'])) {
+                $item['label'] = '';
+            }
+            $encodeLabel        = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
+            $items[$i]['label'] = $encodeLabel ? Html::encode($item['label']) : $item['label'];
+            $hasActiveChild     = false;
+            if (isset($item['items']) && !$items[$i]['isInnerMenu']) {
+                $items[$i]['items'] = $this->normalizeItems($item['items'], $hasActiveChild);
+                if (empty($items[$i]['items']) && $this->hideEmptyItems) {
+                    unset($items[$i]['items']);
+                    if (!isset($item['url'])) {
+                        unset($items[$i]);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return array_values($items);
+    }
+}
